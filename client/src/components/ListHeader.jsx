609251ff8bc93db2d6b16c65466176d44af72fc8
ivabrankovic08 <ivabrankovic2001@gmail.com>
2024-01-28 19:19:17 +0100

    Stilizovan prvi deo

--- client/src/components/ListHeader.jsx
+++ client/src/components/ListHeader.jsx
@@ -1,16 +1,16 @@
-const ListHeader= ({listName}) => {
-  const signOut=()=>{
+const ListHeader = ({ listName }) => {
+  const signOut = () => {
     console.log('Sign out');
   }
-    return (
-      <div className="list-header">
-       <h1>{listName}</h1> 
-       <div className="button-container"> 
-       <button clasName="create"> ADD NEW</button>
-       <button className="signout" onCLick={signOut}>SIGN OUT</button>
-       </div>
+  return (
+    <div className="list-header">
+      <h1>{listName}</h1>
+      <div className="button-container">
+        <button className="create"> ADD NEW</button>
+        <button className="signout" onCLick={signOut}>SIGN OUT</button>
       </div>
-    );
-  }
-  
-  export default ListHeader
+    </div>
+  );
+}
+
+export default ListHeader
