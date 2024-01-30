b623702601f27a142240a90364738480a7eb2d56
ivabrankovic08 <ivabrankovic2001@gmail.com>
2024-01-30 14:01:48 +0100

    Sign out funkcionalnost

--- client/src/components/ListHeader.jsx
+++ client/src/components/ListHeader.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react';
 import Modal from './Modal';
-
+import { useCookies } from 'react-cookie';
 
 
 const ListHeader = ({ listName, getData }) => {
-  
+  const [cookies, setCookie, removeCookie] = useCookies(null)
   const [showModal, setShowModal] = useState(false)
 
   const signOut = () => {
     console.log('Sign out')
-    
+    removeCookie('Email')
+    removeCookie('AuthToken')
+    window.location.reload()
   }
   return (
     <div className="list-header">
@@ -18,7 +20,7 @@ const ListHeader = ({ listName, getData }) => {
         <button className="create" onClick={() => setShowModal(true)}> ADD NEW</button>
         <button className="signout" onClick={signOut}>SIGN OUT</button>
       </div>
-      {showModal && <Modal mode = {'create'} setShowModal = {setShowModal} getData={getData} />}
+      {showModal && <Modal mode={'create'} setShowModal={setShowModal} getData={getData} />}
     </div>
   );
 }
