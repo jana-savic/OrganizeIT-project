9e55244cf6863a639b5cd59136b6e9c91f24ab9f
jana-savic <jana.savic24@gmail.com>
2024-02-01 21:14:27 +0100

    Obezbedjena komponenta role Visitor- pozivanje javnog API-a

--- client/src/pages/Visitor.jsx
+++ client/src/pages/Visitor.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
-/*za kraj!
-visitor stranica 
-log in dugme koje ce voditi na log in stranicu
-quote sa javnog api-ja
-header, footer... */
-const Visitor = () => {
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Visitor() {
+  const [quote, setQuote] = useState({ text: '', author: '' });
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  
+
+  const fetchRandomQuote = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get('https://type.fit/api/quotes');
+      const number= Math.floor(Math.random()*16)
+      const { text, author } = response.data[number];
+      setQuote({ text, author });
+      setLoading(false);
+    } catch (error) {
+      setError('Error fetching quote');
+      setLoading(false);
+      console.error('Error fetching quote: ', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRandomQuote();
+  }, []);
+
+
   return (
-    <div>Visitor</div>
-  )
+    <div>
+      <h1>Random Quote:</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <>
+          <p>{quote.text}</p>
+          <p>- {quote.author}</p>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Visitor
