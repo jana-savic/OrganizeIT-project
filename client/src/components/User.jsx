646050805d1332d831035269d3e9d6e081e87b2e
ivabrankovic08 <ivabrankovic2001@gmail.com>
2024-02-04 17:38:49 +0100

    Omoguceno filtriranje po kategoriji taska

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -11,43 +11,64 @@ const User = () => {
   const userEmail = cookies.Email
   const sortedTasks = tasks?.sort((a, b) => new Date(a.date) - new Date(b.date))
 
+  /*Filtriranje*/
+  const [selectedCategory, setSelectedCategory] = useState('');
+
   const getUser = async () => {
     try {
-        const response = await fetch(`http://localhost:8002/users/${userEmail}`)
-        const json = await response.json()
-        setUser(json[0])
-        setCookie('user_role', json[0].role)
+      const response = await fetch(`http://localhost:8002/users/${userEmail}`)
+      const json = await response.json()
+      setUser(json[0])
+      setCookie('user_role', json[0].role)
     } catch (err) {
-        console.error(err)
+      console.error(err)
     }
-}
+  }
 
-const getData = async () => {
+  const getData = async () => {
     try {
-        const response = await fetch(`http://localhost:8002/todos/${userEmail}`)
-        const json = await response.json()
-        setTasks(json)
+      const response = await fetch(`http://localhost:8002/todos/${userEmail}`)
+      const json = await response.json()
+      setTasks(json)
     } catch (err) {
-        console.error(err)
+      console.error(err)
     }
-}
+  }
 
-useEffect(() => {
-  getUser()
-  getData()
-}, [])
+  /*Promena odabranog u comboBoxu*/
+  const handleCategoryChange = (event) => {
+    const selectedValue = event.target.value;
+    setSelectedCategory(selectedValue === "ALL" ? "" : selectedValue);
+  }
+
+  const filteredTasks = selectedCategory ? sortedTasks?.filter(task => task.category === selectedCategory) : sortedTasks;
+
+  useEffect(() => {
+    getUser()
+    getData()
+  }, [])
 
 
   return (
-    
-        <div>
-    
-    <ListHeader listName={'TO DO :)'} getData={getData} />
-    <p className="user-email">Welcome back {userEmail}</p>
-    {sortedTasks?.map((task) => <ListItem key={task.id} task={task} getData={getData} />)}
+
+    <div>
+
+      <ListHeader listName={'Your list'} getData={getData} />
+      <p className="user-email">Welcome back {userEmail}</p>
+
+      {/* ComboBox */}
+      <select value={selectedCategory} onChange={handleCategoryChange} className='combo-box'>
+        <option className = 'combo-box-item' value="ALL">ALL</option>
+        <option className = 'combo-box-item' value="WORK">WORK</option>
+        <option className = 'combo-box-item' value="HOME">HOME</option>
+        <option className = 'combo-box-item' value="TRAVEL">TRAVEL</option>
+        {/* Add other category options as needed */}
+      </select>
+      {/* Display filtered tasks */}
+      {filteredTasks?.map((task) => <ListItem key={task.id} task={task} getData={getData} />)}
     </div>
-    
-    
+
+
   )
 }
 
